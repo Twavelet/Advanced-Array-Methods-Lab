eb84ffe17a927ad667342badc44e0321887745b3
trevorwavelet@gmail.com <trevorwavelet@gmail.com>
2022-09-06 14:47:58 -0400

    Completed problem six

--- app.js
+++ app.js
@@ -160,21 +160,21 @@ console.log("Vegetarian food from Dishes Array: ", vegetarian)
 //2. Create a function that will prompt the user to enter a cuisine type and then return all dishes that match that type
 //Filter
 
-function problemTwo(){
-    let userInput = prompt ("What type of cuisine type would you like to look up? ").toLowerCase()
-    let userChoice = dishes.filter(function(el){
-        if (el.cuisine.toLowerCase() === userInput){
-            return true;
-        }
-        else{
-            return false;
-        }
-    })
-    return userChoice
-}
+// function problemTwo(){
+//     let userInput = prompt ("What type of cuisine type would you like to look up? ").toLowerCase()
+//     let userChoice = dishes.filter(function(el){
+//         if (el.cuisine.toLowerCase() === userInput){
+//             return true;
+//         }
+//         else{
+//             return false;
+//         }
+//     })
+//     return userChoice
+// }
 
-let userCuisine = problemTwo();
-console.log( "All the dishes you wanted to see are:" ,userCuisine)
+// let userCuisine = problemTwo();
+// console.log( "All the dishes you wanted to see are:" ,userCuisine)
 
 
 //3. Create a function that will return all dishes with the cuisine type of "Italian" and a serving size greater than 5.
@@ -238,7 +238,21 @@ console.log("Dishes with an equal servings count are: ", evenServings)
 //Hint: You do not want to check the array's indexes to find out what the array INCLUDES.
 //Double Hint: Research 'javascript does array include item'
 //Filter
+function problemSix(){
+    let results;
+    results = dishes.filter(function(el){
+        if (el.ingredients.includes("chickpea")){
+            return true;
+        }
+        else {
+            return false;
+        }
+    })
+    return results
+}
 
+let chickpeas = problemSix()
+console.log("All dishes containing chickpeas are: ", chickpeas)
 
 
 //7. Create a function that will prompt the user to type the name of one ingredient. Then use a filter to find all the dishes whose ingredients array INCLUDES that ingredient. Return the new array.
